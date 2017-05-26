34f9e6b73e43c7b48bd21f5d14491fc4752b65dc
Dave W <dviper@gmail.com>
2017-05-25 23:17:02 -0600

    code cleanup for CodeFair

--- test/Pipe-test.js
+++ test/Pipe-test.js
@@ -5,32 +5,32 @@ describe('Pipes', () => {
   const pipe = new Pipe({width: 900}, 300, 100);
 
   it('should be an instance of Pipes', () => {
-    expect(pipe).to.be.an.instanceof(Pipe)
-  })
+    expect(pipe).to.be.an.instanceof(Pipe);
+  });
 
   it('should have a width of 90', () => {
-    expect(pipe.width).to.equal(90)
-  })
+    expect(pipe.width).to.equal(90);
+  });
 
   it('should have a x value of 900', () => {
-    expect(pipe.x).to.equal(900)
-  })
+    expect(pipe.x).to.equal(900);
+  });
 
   it('should have a point value of true', () => {
-    expect(pipe.point).to.equal(true)
-  })
+    expect(pipe.point).to.equal(true);
+  });
 
   it('should have a gapSize value of 300', () => {
-    expect(pipe.gapSize).to.equal(300)
-  })
+    expect(pipe.gapSize).to.equal(300);
+  });
 
   it('should have a padding value of 100', () => {
-    expect(pipe.padding).to.equal(100)
-  })
+    expect(pipe.padding).to.equal(100);
+  });
 
   it('should move to the left at the specified amount', () => {
     expect(pipe.x).to.equal(900);
     pipe.move(100);
     expect(pipe.x).to.equal(800);
-  })
-})
+  });
+});
--- test/Player-test.js
+++ test/Player-test.js
@@ -10,36 +10,36 @@ describe('Player', () => {
   const player = new Player();
 
   it('should be an instance of Player', () => {
-    expect(player).to.be.an.instanceof(Player)
-  })
+    expect(player).to.be.an.instanceof(Player);
+  });
 
   it('should have an x value', () => {
-    expect(player.x).to.equal(110)
-  })
+    expect(player.x).to.equal(110);
+  });
 
   it('should have an y value', () => {
-    expect(player.y).to.equal(300)
-  })
+    expect(player.y).to.equal(300);
+  });
 
   it('should have an width value', () => {
-    expect(player.width).to.equal(30)
-  })
+    expect(player.width).to.equal(30);
+  });
 
   it('should have an height value', () => {
-    expect(player.height).to.equal(30)
-  })
+    expect(player.height).to.equal(30);
+  });
 
   it("should have it's y value increase by 5 when fall is called", () => {
-    expect(player.y).to.equal(300)
-    player.fall({height: 900})
-    expect(player.y).to.equal(305)
-  })
+    expect(player.y).to.equal(300);
+    player.fall({height: 900});
+    expect(player.y).to.equal(305);
+  });
 
   it("should have it's y value decreased by 75 when player.fly is called", () => {
-    expect(player.y).to.equal(305)
-    player.fly()
-    expect(player.y).to.equal(230)
-  })
+    expect(player.y).to.equal(305);
+    player.fly();
+    expect(player.y).to.equal(230);
+  });
 
   it('should collide with pipe when they occupy the same coordinates', () => {
     const canvas = {height: 667, width: 900};
@@ -49,5 +49,5 @@ describe('Player', () => {
     expect(player.detectColllision(canvas, pipeArray)).to.equal(false);
     pipe.move(795);
     expect(player.detectColllision(canvas, pipeArray)).to.equal(true);
-  })
-})
+  });
+});
